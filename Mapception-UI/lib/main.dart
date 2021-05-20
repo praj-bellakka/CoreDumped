402a40210f9a0713b19450cdc288a896af958fbc
praj-bellakka <61443313+praj-bellakka@users.noreply.github.com>
2021-05-20 18:57:37 +0800

    v0.11 - added current location button
    clicking current location button pans the camera to the user's current location

--- Mapception-UI/lib/main.dart
+++ Mapception-UI/lib/main.dart
@@ -1,4 +1,6 @@
 import 'dart:async';
+import 'dart:typed_data';
+import 'package:flutter/services.dart';
 import 'package:location/location.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -29,51 +31,97 @@ class MapScreen extends StatefulWidget {
 class _MapScreenState extends State<MapScreen> {
   //Position _currentPosition; //object to hold location data
   GoogleMapController _controller;
+  StreamSubscription _locationSubscription;
   //Geolocator geolocator = Geolocator();
-  Marker marker;
-  Location location = Location();
-  Circle circle;
-  static const _initialCameraPosition = CameraPosition(
+  Marker marker; //adds the locator icoon to mark current location in real time
+  Location _locationTracker = Location();
+  Circle circle; // adds a circle around the marker for visibility
+  bool flag = false;
+  Map _position;
+  static final _initialCameraPosition = CameraPosition(
     target: LatLng(1.3521, 103.8198),
     zoom: 11.5,
   );
 
-  //_onMapCreated generates an animation to focus on the user's current location
-  // StreamSubscription<Position> homeTabPostionStream;
-  // //_controller = _cntlr;
-  // homeTabPostionStream = Geolocator.getPositionStream(
-  //         desiredAccuracy: LocationAccuracy.bestForNavigation)
-  //     .listen((location) async {
-  //   print(_currentPosition);
-  //   _controller.moveCamera(CameraUpdate.newCameraPosition(CameraPosition(
-  //     target: LatLng(10, 1),
-  //     zoom: 20,
-  //   )));
-  // });
-  // if (_currentPosition != null) {
-  //   _controller.animateCamera(
-  //     CameraUpdate.newCameraPosition(
-  //       CameraPosition(
-  //           target:
-  //               LatLng(_currentPosition.latitude, _currentPosition.longitude),
-  //           zoom: 15),
-  //     ),
-  //   );
-  // }
+  //code to convert icon asset to bitmap
+  Future<Uint8List> getMarker() async {
+    ByteData bytedata =
+        await DefaultAssetBundle.of(context).load("assets/locator.png");
+    return bytedata.buffer.asUint8List();
+  }
 
-  void _onMapCreated(GoogleMapController _cntlr) {
-    _controller = _controller;
-    location.onLocationChanged.listen((l) {
-      _controller.animateCamera(
-        CameraUpdate.newCameraPosition(
-          CameraPosition(target: LatLng(l.latitude, l.longitude), zoom: 15),
-        ),
+  updateMarker(LocationData _locationData, Uint8List imageData) async {
+    LatLng latlng = LatLng(_locationData.latitude, _locationData.longitude);
+    this.setState(() {
+      marker = Marker(
+        markerId: MarkerId("current_location"),
+        position: latlng,
+        draggable: false,
+        flat: true,
+        zIndex: 2,
+        anchor: Offset(0.5, 0.5),
+        icon: BitmapDescriptor.fromBytes(imageData),
+      );
+      circle = Circle(
+        circleId: CircleId("locator-circle"),
+        radius: _locationData.accuracy,
+        zIndex: 1,
+        center: latlng,
+        strokeColor: Colors.lightBlue,
+        fillColor: Colors.blue.withAlpha(80),
       );
     });
   }
 
+//function to get the current location using geocoder api
+  void _gotoCurrentPosition() {
+    if (null != _controller && null != _position) {
+      _controller.animateCamera(
+        CameraUpdate.newCameraPosition(
+          new CameraPosition(
+              bearing: _position["heading"],
+              target: LatLng(_position["lat"], _position["lng"]),
+              tilt: 0,
+              zoom: 14.00),
+        ),
+      );
+    }
+  }
+
+  void _getCurrentLocation() async {
+    print("executing _getCurrentLocation ");
+    try {
+      Uint8List imageData = await getMarker();
+      var currentLocation = await _locationTracker.getLocation();
+
+      updateMarker(currentLocation, imageData);
+      if (_locationSubscription != null) {
+        _locationSubscription.cancel();
+      }
+
+      _locationSubscription = _locationTracker.onLocationChanged
+          .listen((LocationData newLocalData) {
+        setState(() {
+          _position = {
+            "lat": newLocalData.latitude,
+            "lng": newLocalData.longitude,
+            "heading": newLocalData.heading,
+          };
+        });
+        print(newLocalData);
+        updateMarker(newLocalData, imageData);
+      });
+    } on PlatformException catch (error) {
+      if (error.code == 'PERMISSION DENIED') {
+        debugPrint("Permission Denied");
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    print("build ");
+    _getCurrentLocation();
     return Scaffold(
         // appBar: AppBar(
         //     title: Text('Mapception'),
@@ -85,6 +133,8 @@ class _MapScreenState extends State<MapScreen> {
               myLocationButtonEnabled: false,
               zoomControlsEnabled: false,
               initialCameraPosition: _initialCameraPosition,
+              markers: Set.of((marker != null) ? [marker] : []),
+              circles: Set.of((circle != null) ? [circle] : []),
               onMapCreated: (GoogleMapController controller) {
                 _controller = controller;
               },
@@ -117,33 +167,11 @@ class _MapScreenState extends State<MapScreen> {
           ],
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            _getCurrentLocation();
-          },
+          onPressed: _gotoCurrentPosition
+          // ignore: unnecessary_statements
+          ,
           child: Icon(Icons.location_pin),
           backgroundColor: Colors.cyan,
         ));
-  }
-
-
-  void updateMarker(LocationData _locationData) {
-    LatLng latlng = LatLng(_locationData.latitude, _locationData.longitude);
-    this.setState((){
-      marker = Marker(
-        markerId: MarkerId("current_location"),
-        position:latlng,
-        draggable: false,
-        flat: true,
-        //icon: BitmapDescriptor.fromBytes("ADD CUSTOM MARKER HERE")
-
-      );
-    });
-  }
-//function to get the current location using geocoder api
-  void _getCurrentLocation() async {
-    try {
-      var currentLocation = await location.getLocation();
-      
-    } 
   }
 }
