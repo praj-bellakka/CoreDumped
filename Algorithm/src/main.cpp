dfabe47cde0a4d5b67114a1e883dd8bd07e2bb4f
leowyy99 <leowyy99@hotmail.com>
2021-05-21 16:45:18 +0800

    priority queue and edge list

--- Algorithm/src/main.cpp
+++ Algorithm/src/main.cpp
@@ -8,49 +8,68 @@
  */
 #include <iostream>
 #include <list>
+#include <queue>
 
 #define INFINITY 9999
 
 using namespace std;
 
-class nodeWeightPair {
+class edgeListNode 
+{
+    private:
+        int _from;
+        int _to;
+        int _weight;
+
+    public:
+        edgeListNode(int f, int t, int w) { _from = f; _to = t; _weight = w; };
+        edgeListNode(const edgeListNode& eln) { _from = eln._from; _to = eln._to; _weight = eln._weight; };
+        int fromNode() { return _from; };
+        int toNode() { return _to; };
+        int weight() { return _weight; };
+};
+
+class adListNode {
     private:
         int _index;
         int _weight;
 
     public:
-        nodeWeightPair(int n, int w) { _index = n; _weight = w; };
-        nodeWeightPair(const nodeWeightPair& nwp) { _index = nwp._index; _weight = nwp._weight; };
+        adListNode(int n, int w) { _index = n; _weight = w; };
+        adListNode(const adListNode& nwp) { _index = nwp._index; _weight = nwp._weight; };
         int nodeIndex() { return _index; };
         int weight() { return _weight; };
-        //bool operator>(const nodeWeightPair& nwp) { return _weight > nwp._weight;};
-        //bool operator == (const nodeWeightPair& nwp) { return _index == nwp._index; };
+        //bool operator > (adListNode nwp) { return _weight > nwp._weight;};
+        //bool operator < (adListNode nwp) { return _weight > nwp._weight;};
+        //bool operator == (const adListNode& nwp) { return _index == nwp._index; };
 };
+bool operator<(const edgeListNode nwp1, const edgeListNode nwp2)
+{
+    int w1 = nwp1.weight();
+    int w2 = nwp2.weight();
+
+    return w1 > w2;
+}
 
 class Graph
 {
     private:
         int **_matrix;
-        list<nodeWeightPair> *_list;                               //1
+        list<adListNode> *_adList;                               //1
+        list<edgeListNode> _edgeList;
         int _nv; // number of nodes
 
     public: 
+        int size() {return _nv;};
         Graph(int n);
         void addEdge(int s, int d, int w);
         void printMatrix();
-        void printList();
+        void printAdList();
+        void printEdgeList();
+        void minSpanTree(Graph mst);
         ~Graph();
-        //void printGraph();
 };
 
-//struct definitions
-typedef struct 
-{
-    int _index;
-    string _address;
-} location;
-
-void minSpanTree(Graph mst, Graph map);
 int main()
 {
     //read in number of nodes
@@ -73,21 +92,33 @@ int main()
     }
 
     map.printMatrix();
-    map.printList();
+    map.printAdList();
+    map.printEdgeList();
 
     //generate minimum spanning tree mst
     Graph mst(numOfNodes);
-    minSpanTree(mst, map);
+    map.minSpanTree(mst);
 
-    map.~Graph();
 
     return 0;
 }
 
-void minSpanTree(Graph mst, Graph map)
+void Graph::minSpanTree(Graph mst)
 {
-
-    //push into min heap
+    //constructing pq
+    priority_queue<edgeListNode> pq;
+    for (list<edgeListNode>::iterator it = _edgeList.begin(); it != _edgeList.end(); it++)
+    {
+        pq.push(*it);
+    }
+    
+    cout << endl;
+    
+    while (!pq.empty())
+    {
+        cout << "(" << (pq.top()).fromNode() << ", " << (pq.top()).toNode() << ", " << (pq.top()).weight() << ")" << endl;
+        pq.pop();
+    }
     
 
     //loop
@@ -116,7 +147,8 @@ Graph::Graph(int n)
         _matrix[i] = new int [i + 1];
     }
     //construct adjacency list
-    _list = new list<nodeWeightPair> [n];
+    _adList = new list<adListNode> [n];
+
 
 }
 
@@ -151,7 +183,7 @@ void Graph::printMatrix()
 /**
  * To print out the adjacency list to stdout.
  */
-void Graph::printList()
+void Graph::printAdList()
 {
     cout << endl;
     cout << "Adjacency List: " << endl;
@@ -159,11 +191,22 @@ void Graph::printList()
     {
         cout << i << ":\t";
         //print each linked list
-        for (list<nodeWeightPair>::iterator it = _list[i].begin(); it != _list[i].end(); it++)
+        for (list<adListNode>::iterator it = _adList[i].begin(); it != _adList[i].end(); it++)
         {
             cout << "(" << (*it).nodeIndex() << ", " <<(*it).weight() << ")" << "\t";
         }
         cout << endl;
+    }
+}
+
+void Graph::printEdgeList()
+{
+    cout << endl;
+    cout << "Edge List: " << endl;
+
+    for (list<edgeListNode>::iterator it = _edgeList.begin(); it != _edgeList.end(); it++)
+    {
+        cout << "(" << (*it).fromNode() << ", " << (*it).toNode() << ", " << (*it).weight() << ")" << endl;
     }
 }
 
@@ -179,6 +222,8 @@ void Graph::printList()
 void Graph::addEdge(int row, int col, int w)
 {
     _matrix[row][col] = w;
-    nodeWeightPair newNode(col, w);
-   _list[row].push_back(newNode);
+    adListNode newNode(col, w);
+   _adList[row].push_back(newNode);
+   edgeListNode newNode1(row, col, w);
+   _edgeList.push_back(newNode1);
 }
